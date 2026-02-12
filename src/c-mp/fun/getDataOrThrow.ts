95128f4838202b63fce776b75438fbc8e72024f2
András Parditka <andraaspar@gmail.com>
2026-02-12 17:53:13 +0100

    Remove accidental addition.

--- src/c-mp/fun/getDataOrThrow.ts
+++
@@ -1,7 +0,0 @@
-import { appState } from '../../model/appState'
-
-export function getDataOrThrow() {
-	const it = appState.data
-	if (!it) throw new Error(`[tact2x] Data not defined.`)
-	return it
-}
